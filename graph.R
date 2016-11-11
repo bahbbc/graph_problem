5acb09afcee41e903f5f161b00d30a4f3b11c666
Barbara Barbosa <barbarab.claudino@gmail.com>
2016-11-11 17:40:02 -0200

    Add centrality calculation

--- graph.R
+++ graph.R
@@ -1,25 +1,15 @@
 #read file
-graph <- read.csv('~/workspace/graph_challenge/edges.dat', header = TRUE, sep = ' ')
-
-#verify number of rows and columns
-rows <- sort(unique(graph$X48))
-cols <- sort(unique(graph$X64))
-
-relational_matrix <- matrix(0, nrow = 100, ncol = 100)
-
-for(i in 1:dim(graph)[1]){
-  relational_matrix[graph[i,]$X64 + 1, graph[i,]$X48 + 1] <- 1
-  relational_matrix[graph[i,]$X48 + 1, graph[i,]$X64 + 1] <- 1
-  i <- i + 1 
+new_edge <- function(empty_matrix){
+  empty_matrix[graph[i,]$X64 + 1, graph[i,]$X48 + 1] <- 1
+  empty_matrix[graph[i,]$X48 + 1, graph[i,]$X64 + 1] <- 1
+  empty_matrix
 }
 
 ## Dijkstra
 
-distance <- rep(Inf, 100)
-distance[1] <- 0
-min_path <- distance
-names <- 1:100
-Q <- data.frame(names, distance)
+extract_min <- function(Q){
+  Q$name[Q$distance == min(Q$distance)][1]
+}
 
 dijkstra <- function(Q, relational_matrix, min_path){
   while(dim(Q)[1] > 1){
@@ -37,13 +27,47 @@ dijkstra <- function(Q, relational_matrix, min_path){
   min_path
 }
 
-real_min_path <- dijkstra(Q, relational_matrix, min_path)
-
-
-
-extract_min <- function(Q){
-  Q$name[Q$distance == min(Q$distance)][1]
+generate_distance_matrix <- function(relational_matrix, distance_matrix){
+  for(j in 1:dim(relational_matrix)[1]){
+    distance <- rep(Inf, dim(relational_matrix)[1])
+    distance[j] <- 0
+    names <- 1:dim(relational_matrix)[1]
+    min_path <- distance
+    
+    Q <- data.frame(names, distance)
+    
+    real_min_path <- dijkstra(Q, relational_matrix, min_path)
+    distance_matrix <- rbind(distance_matrix, real_min_path)
+  }
+  colnames(distance_matrix) <- 1:dim(relational_matrix)[1]
+  distance_matrix
 }
+
+centrality <- function(distance_matrix){
+  1/rowSums(distance_matrix)
+}
+
+graph <- read.csv('~/workspace/graph_challenge/edges.dat', header = TRUE, sep = ' ')
+
+distance <- rep(Inf, 100)
+distance[1] <- 0
+min_path <- distance
+names <- 1:100
+Q <- data.frame(names, distance)
+
+#verify number of rows and columns
+rows <- sort(unique(graph$X48))
+cols <- sort(unique(graph$X64))
+
+relational_matrix <- matrix(0, nrow = 100, ncol = 100)
+
+for(i in 1:dim(graph)[1]){
+  relational_matrix <- new_edge(relational_matrix)
+  i <- i + 1 
+}
+
+final_distance_matrix <- generate_distance_matrix(relational_matrix, distance_matrix)
+centrality(final_distance_matrix)
 
 ####
 # test 1
@@ -63,11 +87,15 @@ for(i in 1:dim(graph)[1]){
 }
 
 relational_matrix
-distance <- rep(Inf, 6)
-min_path <- distance
-distance[1] <- 0
-names <- 1:6
 
-Q <- data.frame(names, distance)
+distance_matrix <- data.frame()
+
+final_distance_matrix <- generate_distance_matrix(relational_matrix, distance_matrix)
+
+
+
+
+
+
 
 
